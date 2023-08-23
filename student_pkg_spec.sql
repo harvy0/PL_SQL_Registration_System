c1a937b72651dd58f8dc45f247bf3af918d5d684
harvy0 <93056706+harvy0@users.noreply.github.com>
2023-08-22 20:00:47 -0700

    version 2 of student_pkg

---
+++ student_pkg_spec.sql
@@ -0,0 +1,41 @@
+--------------------------------------------------------
+--  File created - Tuesday-August-22-2023   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Package STUDENT_PKG
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE PACKAGE "HARVY"."STUDENT_PKG" AS
+
+  
+
+  ----------Overloading procedures with same name but different parameters------------
+  
+  
+  ----------------Search a student by Student ID-----------------
+  PROCEDURE p_view_student (p_student_id in student.student_id%type);
+
+
+  ---------------Method overloaded by using same name but different data type Parameters-----------------
+
+  ----------------Search a student by Student Email--------------
+  PROCEDURE p_view_student (p_student_email in student.email%type);
+
+
+  ---------------------Delete a student------------------------
+  PROCEDURE p_delete_student (p_student_id in student.student_id%type);
+
+
+  ----------------------UPDATE Student--------------------------
+  PROCEDURE p_update_student (p_student_id in student.student_id%type,
+                              v_field_name in VARCHAR2,
+                              v_value in VARCHAR2);
+
+
+  -----------------------Get Student Record---------------------
+  PROCEDURE p_student_record (p_student_id in student.student_id%type);
+
+
+END student_pkg;
+
+/
